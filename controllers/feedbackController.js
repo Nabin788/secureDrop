2c827540b2a8c9de57f95363d78acd7363c9666e
Nabin Poudel <nabinpoudel788@gmail.com>
2025-06-12 15:12:46 +0545

    created feedbackController for feedbackUser and it's status

---
+++ controllers/feedbackController.js
@@ -0,0 +1,34 @@
+const feedbackModels = require("../models/feedbackModel.js");
+
+const feedbackUser = async (req, res) => {
+    try {
+        const { text } = req.body;
+        if (!text) {
+            return res.status(401).send("Please Insert text.");
+        }
+
+        const feedback = feedbackModels({
+            text,
+            userId: req.user._id
+        });
+
+        await feedback.save();
+
+        res.status(201).send({ message: "Feedback submit successfully" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+const feedbackStatus = async (req,res) => {
+    try {
+        const feedStatus = await feedbackModels({userId: req.user._id}).select("text");
+        res.json(feedStatus);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({error: error.message});
+    }
+}
+
+module.exports = {feedbackUser, feedbackStatus};
