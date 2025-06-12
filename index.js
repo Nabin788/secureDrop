8b723954a7e76910be1a10c5937e2f00de745444
Nabin Poudel <nabinpoudel788@gmail.com>
2025-06-12 12:13:57 +0545

    import and define user and admin routes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // Import Express module
 const express = require("express");
+
+//Import user and admin file
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes.js");
 // create an Instance of express application
 const app = express();
 
 // creating server port
 const port = 1010;
+
+// define admin routes
+app.use("/admin", adminRoutes);
+
+//define user routes
+app.use("/user", userRoutes);
 
 // Start the server and have it listen on port 1010 for incoming request
 app.listen(port, () => {
