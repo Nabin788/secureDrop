e759d7a9d58cc43e78cf1381d94ae129e5af177d
Nabin Poudel <nabinpoudel788@gmail.com>
2025-06-13 16:25:32 +0545

    define auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,11 @@
 // create a router instance from express module
 const routes = require("express").Router();
 
-// define GET routes for the root url "/"
-routes.get("/", (req,res) => {
-    res.send("Welcome to secure Drop, where you can write complain, feedback and report without reveling their identity.");
-});
+const  { registerUser, loginUser } = require("../controllers/authController.js");
 
+// define GET routes for the root url "/"
+routes.post("/register", registerUser);
+routes.post("/login", loginUser);
 
 // export the routes
 module.exports = routes;
