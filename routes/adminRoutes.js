7987e8fec0e9dee39af22091e52324d3d2252841
Nabin Poudel <nabinpoudel788@gmail.com>
2025-06-13 16:24:18 +0545

    define feedback delete routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,12 @@
 // Creating Router Instance from express module 
 const routes = require("express").Router();
 
+const { feedbackUser, feedbackStatus, feedbackDelete } = require("../controllers/adminController.js");
+
 // create routes for admin
-routes.get("/", (req,res) => {
-    res.send("Welcome to admin roots");
-});
+routes.get("/feedback", feedbackUser);
+routes.patch("/feedback/:id/respond", feedbackStatus);
+routes.delete("/feedback/:id", feedbackDelete);
 
 // exporting admin routes
 module.exports = routes;
