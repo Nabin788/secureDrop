0b03a134bf19aef65660877f37e9b11871a42cbd
Nabin Poudel <nabinpoudel788@gmail.com>
2025-06-13 16:24:56 +0545

    define feedback routes

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,11 +1,12 @@
 // create a router instance from express module
 const routes = require("express").Router();
 
-// define GET routes for the root url "/"
-routes.get("/", (req,res) => {
-    res.send("Welcome to secure Drop, where you can write complain, feedback and report without reveling their identity.");
-});
+const { feedbackStatus, feedbackUser } = require("../controllers/feedbackController.js");
+const { authMiddleware } = require("../middleware/authMiddleware.js");
 
+// define GET routes for the root url "/"
+routes.post("/submit", authMiddleware, feedbackUser);
+routes.get("/status", authMiddleware, feedbackStatus);
 
 // export the routes
 module.exports = routes;
