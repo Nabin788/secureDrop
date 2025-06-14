079fa0c0bc0209674d5c3cb97b0518a0dcfb054c
Nabin Poudel <nabinpoudel788@gmail.com>
2025-06-14 21:30:09 +0545

    Password changed routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 // create a router instance from express module
 const routes = require("express").Router();
 
-const  { registerUser, loginUser } = require("../controllers/authController.js");
+const  { registerUser, loginUser, forgetPassword } = require("../controllers/authController.js");
 
 // define GET routes for the root url "/"
 routes.post("/register", registerUser);
 routes.post("/login", loginUser);
+routes.put("/forgetPassword", forgetPassword);
 
 // export the routes
 module.exports = routes;
