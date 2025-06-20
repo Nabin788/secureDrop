27539430307311183adc3e993118a15413e37998
Nabin Poudel <nabinpoudel788@gmail.com>
2025-06-20 23:05:00 +0545

    fix cookie error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,25 @@
 const jwt = require("jsonwebtoken");
 const userModels = require("../models/userModel.js");
 const mail = require("../nodemailer/config.js");
+const cookie = require("cookie-parser");
 
-function createToken(user) {
-    return jwt.sign({ _id: user._id }, process.env.SECRETKEY, { expiresIn: "1h" });
+function createToken(user, res) {
+
+    const token = jwt.sign({ _id: user._id }, process.env.SECRETKEY, { expiresIn: "1h" });
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 60*60*1000
+    });
+    return token;
 }
 
 const registerUser = async (req, res) => {
 
     try {
         const { email, password } = req.body;
+        
         if (!email || !password) {
             return res.status(401).json({ message: "All field are required" });
         }
@@ -40,6 +50,7 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
+        console.log(email, password);
 
         if (!email || !password) {
             return res.status(401).json({ message: "All field require." });
@@ -50,7 +61,10 @@ const loginUser = async (req, res) => {
             return res.status(401).json({ message: "Invalid user." });
         }
 
+        console.log(password, userPassword);
+
         const userPassword = await users.comparePassword(password);
+        
         if (!userPassword) {
             return res.status(401).json({ message: "Invalid password" });
         }
@@ -58,7 +72,7 @@ const loginUser = async (req, res) => {
         const generateToken = createToken(users);
         res.status(200).json({ message: "User login sucessfully.", token: generateToken });
     } catch (error) {
-        console.error(error.message);
+        console.error("Unable to login user", error.message);
         res.status(500).json({ message: "server error", error: error.message });
     }
 
@@ -73,12 +87,11 @@ const forgetPassword = async (req, res) => {
 
         if(password !== confirmPassword) return res.status(401).json({message: "Passowrd and confirm password not match "});
 
-        const userEmail = await userModels.findOne({email});
-        if(!userEmail) return res.status(404).json({message: "Invalid user."});
+        const user = await userModels.findOne({email});
+        if(!user) return res.status(404).json({message: "Invalid user."});
 
-        const changePassword = userModels({
-            password
-        });
+        user.password = password;
+        await user.save();
 
         res.status(201).send({message: "Password Change successfull.", changePassword});
     } catch (error) {
